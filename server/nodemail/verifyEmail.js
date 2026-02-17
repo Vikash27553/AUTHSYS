1506f5d7ecea81186655d4662b2901d8de17ab54
Vikash Kaushik <vikashkaushik199@gmail.com>
2026-02-17 15:46:18 +0530

    container update

--- server/nodemail/verifyEmail.js
+++ server/nodemail/verifyEmail.js
@@ -27,7 +27,7 @@ import nodemailer from 'nodemailer';
  const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: false, // true for 465, false for other ports
+    secure: true, // true for 465, false for other ports
     auth:{
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD
