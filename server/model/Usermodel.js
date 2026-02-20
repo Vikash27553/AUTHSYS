a1d4b8dab0d56989f9e9fe405089d65f447ed49e
Vikash Kaushik <vikashkaushik199@gmail.com>
2026-02-20 15:13:50 +0530

    update Usermodel.js

--- server/model/Usermodel.js
+++ server/model/Usermodel.js
@@ -7,10 +7,10 @@ const userSchema = new mongoose.Schema({
     password:{type:String , required: true},
     isVerfied:{type:Boolean, default: false},
     isLoggedin:{type:Boolean, default:false},
-    token:{type:String,default: null, unique:true},
+    token:{type:String,default: null,select:false} ,
     otp:{type:String, default:null},
     otp_expiry:{type:Date, default:null}
 
-})
+}, {timestamps:true});
 
 export const User = mongoose.model('user', userSchema);
